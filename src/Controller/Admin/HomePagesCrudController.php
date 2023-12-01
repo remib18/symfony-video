eb80ad2e6ecde23cde297ebf50cf4b1620de39c1
Roosewald <118015442+Roosewald@users.noreply.github.com>
2023-12-01 17:11:13 +0100

    Ajout sécurité webmaster

--- src/Controller/Admin/HomePagesCrudController.php
+++ src/Controller/Admin/HomePagesCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\HomePages;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -21,6 +23,20 @@ class HomePagesCrudController extends AbstractCrudController
             TextField::new('label'),
             TextField::new('markdown')->setFormType(TextareaType::class)->setRequired(true),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions
+            ->disable(Action::NEW, Action::DELETE, Action::SAVE_AND_RETURN)
+            ->update(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE, function (Action $action) {
+                return $action->setIcon('fa fa-save')->setLabel('Save');
+            });
+
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            $actions->disable(Action::NEW, Action::EDIT, Action::DELETE, Action::INDEX);
+        }
+        return $actions;
     }
 
     public function configureCrud(Crud $crud): Crud
