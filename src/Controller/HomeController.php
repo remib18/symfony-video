505810f2bd5f1e12a6e14591047510a08b2597fb
Roosewald <118015442+Roosewald@users.noreply.github.com>
2023-12-10 19:45:26 +0100

    réglage problème modification et suppression possibilité de supprimer la page active (#26)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -55,10 +55,6 @@ class HomeController extends AbstractController
             $htmlContent = $converter->convert($markdownContent);
         } catch (CommonMarkException) {
             $htmlContent = '<p>Une erreur s\'est produite lors de la conversion du contenu.</p>';
-
-
-
-
         }
 
         $contact= new Contact();
